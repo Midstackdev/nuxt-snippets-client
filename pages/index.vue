d6abf4888f270d4b8cc5048dc3a80c079ba06dab
Midstackdev <midstackdev@gmail.com>
2020-03-11 12:00:46 +0000

    setup and base styling

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1 class="font-medium text-4xl">Snippets</h1>
+    <p>By codecourse <a href="#">for all</a></p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+    //
+  }
+}
+</script>
+
