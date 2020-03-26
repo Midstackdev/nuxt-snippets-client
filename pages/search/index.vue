1ce3fc81d3e3a5ab595860d149d7e269e114f989
Midstackdev <midstackdev@gmail.com>
2020-03-26 15:08:21 +0000

    Full vue instant search

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -3,16 +3,52 @@
         <div>
             <div class="bg-white mb-16">
                 <div class="container py-10 pb-16">
-                    <h1 class="text-4xl text-gray-700 font-medium leading-tight mb-4">
-                        Search
-                    </h1>
+                    <div class="flex justify-between items-center mb-6">
+                        <h1 class="text-4xl text-gray-700 font-medium leading-tight mb-4">
+                            Search
+                        </h1>
+                        <client-only>
+                            <AisPoweredBy />
+                        </client-only>
+                    </div>
+
+                    <AisSearchBox
+                        placeholder="Search snippets"
+                        :class-names="{
+                            'ais-SearchBox': 'w-full',
+                            'ais-SearchBox-input': 'w-full border-2 border-gray-400 rounded-lg block p-4 text-lg outline-none',
+                            'ais-SearchBox-submit': 'hidden',
+                            'ais-SearchBox-submitIcon': 'hidden',
+                            'ais-SearchBox-reset': 'hidden',
+                        }"
+                    />
+                    
                 </div>
             </div>
 
             <div class="container">
-                <h1 class="text-xl text-gray-600 font-medium mb-6">
-                    Snippets ()
-                </h1>
+                <AisStateResults>
+                    <div slot-scope="{ query }">
+                        <template v-if="query.length">
+                            <AisStats>
+                                <h1 
+                                    class="text-xl text-gray-600 font-medium mb-6"
+                                    slot-scope="{ nbHits }"
+                                >
+                                    Snippets ({{ nbHits }})
+                                </h1>
+                            </AisStats>
+
+                            <AisHits>
+                                <div slot="item" slot-scope="{ item }">
+                                   <SearchSnippetCard 
+                                        :snippet="item.data"
+                                   />
+                                </div>
+                            </AisHits>
+                        </template>
+                    </div>
+                </AisStateResults>
             </div>
 
         </div>
@@ -20,11 +56,25 @@
 </template>
 
 <script>
-    import { AisInstantSearchSsr } from 'vue-instantsearch'
+    import { 
+        AisInstantSearchSsr, 
+        AisHits, 
+        AisSearchBox, 
+        AisPoweredBy, 
+        AisStats, 
+        AisStateResults, 
+    } from 'vue-instantsearch'
+    import SearchSnippetCard from './components/SearchSnippetCard'
 
     export default {
         components: {
-            AisInstantSearchSsr
+            AisInstantSearchSsr,
+            AisHits,
+            AisSearchBox,
+            AisPoweredBy,
+            AisStats,
+            AisStateResults,
+            SearchSnippetCard
         },
 
         head () {
